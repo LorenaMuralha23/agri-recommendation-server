ac57bcfd90e2557d219ad428de72b27e7d119347
Lorena Muralha <mlmuralha2301@gmail.com>
2024-11-12 19:12:09 -0300

    feat: estrutura spring do projeto

---
+++ demo/src/main/java/com/trabalho_ia/demo/AgroServerApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho_ia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroServerApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/trabalho_ia/demo/AgroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho_ia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
