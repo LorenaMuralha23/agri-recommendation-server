20c1b00308e127d0d0a85586310c53f8e309829a
Lorena Muralha <mlmuralha2301@gmail.com>
2024-11-15 17:42:16 -0300

    fix: calculo da dose de calcario corrigido

--- demo/src/main/java/com/trabalho_ia/demo/controllers/RecomendacaoController.java
+++ demo/src/main/java/com/trabalho_ia/demo/controllers/RecomendacaoController.java
@@ -2,7 +2,6 @@ package com.trabalho_ia.demo.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.trabalho_ia.demo.entities.UserInfo;
 import com.trabalho_ia.demo.services.CalcarioService;
 import jakarta.annotation.PostConstruct;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -34,16 +32,8 @@ public class RecomendacaoController {
     @PostConstruct
     public void testing() {
         CalcarioService service = new CalcarioService();
-        float SMPmacieira = service.definePRNT(4.61f, 5.5f);
-        float SMPconsorciacao = service.definePRNT(5.34f, 6.0f);
-        float NCmac = service.calculaNC(0.65f, 82.42f, 19.52f);
-        float NCcons = service.calculaNC(0.75f, 82.42f, 19.52f);
-        //NC = [(0.65-82.42)/100] x 19.;
-        //float v1, float bases, float CTC
-        System.out.println("NC macieira: " + NCmac);
-        System.out.println("NC consorciacao: " + NCcons);
-
-        System.out.println("Dose de calcário macieira: " + service.calculaDoseCalcario(true, (float) 154.2, (float) 20.2, (float) 6.21, (float) 82.42, (float) 19.52));
+        System.out.println("Dose de calcário macieira: " + service.calculaDoseCalcario(true, 6.945f));
+        System.out.println("Dose de calcário consorciacao: " + service.calculaDoseCalcario(false, 5.51f));
     }
 
 }
